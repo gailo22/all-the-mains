eb68cd20ef0980c6c1840b7f00e838cc949c28b8
gailo22 <gailo22@gmail.com>
2017-06-21 17:32:58 +0800

    updated

--- src/main/java/com/gailo22/Main6.java
+++ src/main/java/com/gailo22/Main6.java
@@ -1,9 +1,36 @@
 package com.gailo22;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main6 {
 
 	public static void main(String[] args) {
 		
+		Runnable t1 = new MyTask("task1");
+		Runnable t2 = new MyTask("task2");
+		Runnable t3 = new MyTask("task3");
+		
+		ExecutorService pool = Executors.newFixedThreadPool(4);
+		pool.submit(t1);
+		pool.submit(t2);
+		pool.submit(t3);
+		
+		pool.shutdown();
+		
 	}
 
+}
+
+class MyTask implements Runnable {
+	private String name;
+	
+	MyTask(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		System.out.println(name + ": " + Thread.currentThread().getName());
+	}
 }
